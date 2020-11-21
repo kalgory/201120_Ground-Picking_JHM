9bc205d998d80d44215a8e80110ff2b9ac23069f
hyunmindev <jung@hyunmin.dev>
2020-11-21 14:36:20 +0900

    Connect with local repository

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+//
+//  main.cpp
+//  ground-picking
+//
+//  Created by 정현민 on 2020/11/21.
+//
 
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, JHM!\n";
+  return 0;
+}
