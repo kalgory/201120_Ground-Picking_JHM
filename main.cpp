26fe8a366d38f9221226dc01e25b3ee8a697a4b9
Hyunmin Jung <jung@hyunmin.dev>
2020-11-22 16:51:41 +0900

    Implement with memoization

--- main.cpp
+++ main.cpp
@@ -10,33 +10,38 @@
 
 using namespace std;
 vector<vector<int>> land_vec_vec;
+vector<vector<int>> memo_vec_vec;
 vector<int>::size_type row_size;
 vector<int>::size_type col_size;
 
-int getMax(int max, int row, int col)
+int getMax(int row, int col)
 {
   if (row == row_size)
   {
     return 0;
   }
-  cout << row << " - " << col << " - " << max << endl;
-  
-  int max_temp = 0;
-  for(int i = 0; i < col_size; i++)
+  if (memo_vec_vec[row][col] != 0)
   {
-    if(i != col)
+    return memo_vec_vec[row][col];
+  }
+  
+  int max = 0;
+  
+  for (int i = 0; i < col_size; i++)
+  {
+    if (i == col)
     {
       continue;
     }
-    int temp = getMax(max + land_vec_vec[row][col], row + 1, i);
-    if(max_temp < temp)
+    int max_temp = getMax(row + 1, i);
+    
+    if (max < max_temp)
     {
-      max_temp = temp;
+      max = max_temp;
     }
-
   }
-  
-  return max_temp;
+  memo_vec_vec[row][col] = land_vec_vec[row][col] + max;
+  return memo_vec_vec[row][col];
 }
 
 int solution(vector<vector<int>> land_vec_vec_temp)
@@ -44,14 +49,24 @@ int solution(vector<vector<int>> land_vec_vec_temp)
   land_vec_vec = land_vec_vec_temp;
   row_size = land_vec_vec.size();
   col_size = land_vec_vec[0].size();
-  int output = getMax(0, 0, 0);
+  memo_vec_vec.resize(row_size, vector<int> (col_size));
+  
+  int output = 0;
+  for (int i = 0; i < col_size; i++) // 각각의 열에서 출발
+  {
+    int max_temp = getMax(0, i);
+    if (output <max_temp)
+    {
+      output = max_temp;
+    }
+  }
   
   return output;
 }
 
 int main(int argc, const char * argv[]) {
   vector<vector<int>> input_vec_vec;
-  input_vec_vec = {{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}, {7, 6, 5, 4}};
+  input_vec_vec = {{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}};
   
   int output = solution(input_vec_vec);
   cout << output << endl;
